7c73056024a09af4e7657232545cf9eaa0761913
bychen <874742015@qq.cpm>
2018-04-25 14:13:21 +0800

    人员审核

--- src/components/recruitDetails.vue
+++ src/components/recruitDetails.vue
@@ -1,45 +1,36 @@
 <template>
   <div class="mainBox">
     <div class="volTableTitleBox">招募活动详情</div>
-    <el-form class="adminTable" label-width="130px">
+    <el-form class="adminTable" label-width="100px">
       <el-form-item label="活动名称">
         <span>{{details.name}}</span>
       </el-form-item>
       <el-form-item label="组织名称">
         <span>{{details.organizename}}</span>
       </el-form-item>
-      <el-form-item label="开放类型">
-        <span>{{details.idcard}}</span>
-      </el-form-item>
-      <el-form-item label="签到情况">
+      <el-form-item label="招募情况">
         <span>{{details.signnum}}/{{details.num}}</span>
       </el-form-item>
       <el-form-item label="活动类型">
-        <span>{{details.phone}}</span>
+        <span>{{details.servicetype}}</span>
       </el-form-item>
       <el-form-item label="活动地址">
         <span>{{details.address}}</span>
       </el-form-item>
+      <el-form-item label="活动招募时间">
+        <span>{{details.restarttime}} - {{details.reendtime}}</span>
+      </el-form-item>
       <el-form-item label="活动进行时间">
-        <span>{{details.phone}}</span>
-      </el-form-item>
-      <el-form-item label="活动签到/签退时间">
-        <span>{{details.phone}}</span>
-      </el-form-item>
-      <el-form-item label="签到地点">
-        <span>{{details.phone}}</span>
-      </el-form-item>
-      <el-form-item label="签到范围">
-        <span>{{details.phone}}</span>
+        <span>{{details.acbegintime}} - {{details.acendtime}}</span>
       </el-form-item>
       <el-form-item label="发布人姓名">
         <span>{{details.phone}}</span>
       </el-form-item>
       <el-form-item label="联系电话">
-        <span>{{details.phone}}</span>
+        <span>{{details.content}}</span>
       </el-form-item>
       <el-form-item label="活动详情">
-        <span>{{details.phone}}</span>
+        <span v-html="details.details"></span>
       </el-form-item>
     </el-form>
   </div>
--- src/components/sginDetails.vue
+++ src/components/sginDetails.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="mainBox">
     <div class="volTableTitleBox">签到活动详情</div>
-    <el-form class="adminTable" label-width="100px">
+    <el-form class="adminTable" label-width="130px">
       <el-form-item label="活动名称">
         <span>{{details.name}}</span>
       </el-form-item>
       <el-form-item label="组织名称">
         <span>{{details.organizename}}</span>
       </el-form-item>
-      <el-form-item label="招募情况">
+      <el-form-item label="开放类型">
+        <span v-if="details.opentype==1">全体开放</span>
+        <span v-if="details.opentype==2">组织开放</span>
+        <span v-if="details.opentype==3">关联活动</span>
+      </el-form-item>
+      <el-form-item label="签到情况">
         <span>{{details.signnum}}/{{details.num}}</span>
       </el-form-item>
       <el-form-item label="活动类型">
@@ -17,23 +22,26 @@
       <el-form-item label="活动地址">
         <span>{{details.address}}</span>
       </el-form-item>
-      <el-form-item label="活动招募时间">
+      <el-form-item label="活动进行时间">
+        <span>{{details.begintime}} - {{details.endtime}}</span>
+      </el-form-item>
+      <el-form-item label="活动签到/签退时间">
+        <span>{{details.signbegintime}} - {{details.signendtime}}</span>
+      </el-form-item>
+      <el-form-item label="签到地点">
         <span>{{details.phone}}</span>
       </el-form-item>
-      <el-form-item label="活动进行时间">
+      <el-form-item label="签到范围">
         <span>{{details.phone}}</span>
       </el-form-item>
       <el-form-item label="发布人姓名">
-        <span>{{details.phone}}</span>
+        <span>{{details.content}}</span>
       </el-form-item>
       <el-form-item label="联系电话">
         <span>{{details.phone}}</span>
       </el-form-item>
-      <el-form-item label="是否开放招募">
-        <span>{{details.phone}}</span>
-      </el-form-item>
       <el-form-item label="活动详情">
-        <span>{{details.phone}}</span>
+        <span v-html="details.details"></span>
       </el-form-item>
     </el-form>
   </div>
