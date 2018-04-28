5699981d3753148c06082c83bcb7407b13e8c3cd
bychen <874742015@qq.cpm>
2018-04-28 17:41:57 +0800

    导航bug

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -4,12 +4,10 @@
 
 <template>
   <el-menu
-    default-active="1"
+    :default-active="activeIndex"
     :unique-opened="true"
     :router="true"
-    active-text-color="#06c1ad"
-    @open="handleOpen"
-    @close="handleClose">
+    active-text-color="#06c1ad">
     <el-menu-item index="1" :route="{name:'indexdefault'}">
       <i class="chen-chenhome"></i>
       <span slot="title">首页</span>
@@ -38,7 +36,6 @@
         <el-menu-item index="3-2" :route="{name:'sginListManage'}">签到人员列表</el-menu-item>
       </el-menu-item-group>
     </el-submenu>
-
     <el-submenu index="4">
       <template slot="title">
         <i class="chen-chenrenshu"></i>
@@ -50,9 +47,6 @@
       <el-menu-item-group>
         <el-menu-item index="4-2" :route="{name:'volCheck'}">志愿者审核</el-menu-item>
       </el-menu-item-group>
-     <!-- <el-menu-item-group>
-        <el-menu-item index="4-3" :route="{name:'volHonorManage'}">富文本编辑器</el-menu-item>
-      </el-menu-item-group>-->
     </el-submenu>
     <el-submenu index="5">
       <template slot="title">
@@ -66,21 +60,6 @@
         <el-menu-item index="5-2" :route="{name:'systemAdmin'}">管理员详情</el-menu-item>
       </el-menu-item-group>
     </el-submenu>
-    <!--<el-submenu index="4">
-      <template slot="title">
-        <i class="el-icon-document"></i>
-        <span>数据统计</span>
-      </template>
-      <el-menu-item-group>
-        <el-menu-item index="4-1">组织数据统计</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group>
-        <el-menu-item index="4-2">活动数据统计</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group>
-        <el-menu-item index="4-3">异常数据统计</el-menu-item>
-      </el-menu-item-group>
-    </el-submenu>-->
   </el-menu>
 </template>
 
@@ -89,15 +68,24 @@
     name: 'navbar',
     data () {
       return {
+        activeIndex:'1',
         msg: 'navbar'
       }
     },
     methods:{
-      handleOpen () {
 
-      },
-      handleClose () {
-
+    },
+    watch:{
+      $route:{
+        handler(newValue, oldValue) {
+          if(newValue.name=='recruitListManage'){
+            this.activeIndex='2-2';
+          }else if(newValue.name=='sginListManage'){
+            this.activeIndex='3-2';
+          }else if(newValue.name=='volCheck'){
+            this.activeIndex='4-2';
+          }
+        }
       }
     }
   }
