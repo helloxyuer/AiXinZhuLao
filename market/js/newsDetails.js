d6a4019b01e6ad734a31a90dd30d7efaaf0ff06a
bychen <874742015@qq.cpm>
2018-07-16 11:24:21 +0800

    二期

--- market/js/newsDetails.js
+++ market/js/newsDetails.js
@@ -6,8 +6,8 @@ var app = new Vue({
   el: '#app',
   data: {
     news:{},
-    iszan:false,
-    isfollow:false
+    iszan:0,
+    isfollow:0
   },
   methods:{
     getDetails:function(){
@@ -21,6 +21,51 @@ var app = new Vue({
         },
         success: function (res) {
           if(res.code==0){
+            self.news = res.data.news;
+            self.isfollow = res.data.isfollow;
+            self.iszan = res.data.iszan;
+          }else{
+            showTips(res.msg)
+          }
+        }
+      })
+    },
+    guanzhu:function () {
+      var self = this;
+      passData({
+        method:'post',
+        type:'json',
+        url:'app/vol/follow',
+        data: {
+          aboutid:self.news.uuid,
+          operationtype:self.isfollow==0?1:2,
+          abouttype:6,
+          type:2,
+        },
+        success: function (res) {
+          if(res.code==0){
+            self.isfollow==0?self.isfollow=1:self.isfollow=0;
+          }else{
+            showTips(res.msg)
+          }
+        }
+      })
+    },
+    dianzan:function () {
+      var self = this;
+      passData({
+        method:'post',
+        type:'json',
+        url:'app/vol/follow',
+        data: {
+          aboutid:self.news.uuid,
+          operationtype:self.iszan==0?1:2,
+          abouttype:6,
+          type:1,
+        },
+        success: function (res) {
+          if(res.code==0){
+            self.iszan==0?self.iszan=1:self.iszan=0;
           }else{
             showTips(res.msg)
           }
